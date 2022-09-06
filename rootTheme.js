816d20958a24f0e619ce8c9c1ec6c607ac2a929c
kreatiff <oleg.estrin@gmail.com>
2022-09-06 18:00:02 +1000

    added root theme

---
+++ rootTheme.js
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////
+// Javascript Loader                              //
+////////////////////////////////////////////////////
+
+const loadJS = (FILE_URL, async = true, type = "text/javascript") => 
+{
+return new Promise((resolve, reject) =>
+{
+    try 
+    {
+        const scriptEle = document.createElement("script");
+        scriptEle.type = type;
+        scriptEle.async = async;
+        scriptEle.src =FILE_URL;
+
+        scriptEle.addEventListener("load", (ev) => {
+            resolve({ status: true });
+        });
+
+        scriptEle.addEventListener("error", (ev) => {
+            reject({
+                status: false,
+                message: `Failed to load the script ${FILE_URL}`
+            });
+        });
+
+        document.head.appendChild(scriptEle);
+    } catch (error) {
+        reject(error);
+    }
+});
+};
+
+////////////////////////////////////////////////////
+// END Javascript Loader                          //
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+// CSS Loader                                     //
+////////////////////////////////////////////////////
+
+const loadCSS = (filename) =>
+{
+ var fileref = document.createElement("link");
+ fileref.setAttribute("rel", "stylesheet");
+ fileref.setAttribute("type", "text/css");
+ fileref.setAttribute("href", filename);
+
+ document.getElementsByTagName("head")[0].appendChild(fileref);
+}
+
+////////////////////////////////////////////////////
+// END CSS Loader                                 //
+////////////////////////////////////////////////////
