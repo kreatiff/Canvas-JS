a64dbd80eb1324e3e8df8c0f4607a2bba9068435
Oleg Estrin <oleg.estrin@gmail.com>
2022-09-08 21:43:34 +1000

    Check for document loaded

--- init.js
+++ init.js
@@ -1,5 +1,7 @@
 loadJS(`${hostUrl}/iframeResizer.min.js`).then(
     function() {
         console.log('Resizer loaded');
-        iFrameResize({ log: true }, '#myIframe').then(console.log('resize executed'));
+        if (document.readyState !== 'loading') {
+            iFrameResize({ log: true }).then(console.log('resize executed'));
+        };
     });
