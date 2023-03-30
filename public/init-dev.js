ce2d36b7acdc1ee1ac12157962dd000d73936bca
Oleg Estrin <oleg.estrin@gmail.com>
2023-03-30 16:18:17 +1000

    Change search to case insensitive.

--- public/init-dev.js
+++ public/init-dev.js
@@ -3,7 +3,7 @@ const addStackleClass = () => {
     const container = document.querySelector(".tool_content_wrapper");
 
     // Check if the container has a child element with attribute "data-tool-id" equal to "staging.stackle.app"
-    const hasStackleIntegration = container.querySelector("[data-tool-id='staging.stackle.app']") !== null;
+    const hasStackleIntegration = container.querySelector("[data-tool-id*='stackle.app']") !== null;
 
     // If the container has a child element with attribute "data-tool-id" equal to "staging.stackle.app",
     // add a class "stackle_integration" to the container element
@@ -18,8 +18,9 @@ const detectStackleModule = () => {
     for (let i = 0; i < dialogContainers.length; i++) {
         const childElements = dialogContainers[i].querySelectorAll('*');
         for (let j = 0; j < childElements.length; j++) {
-            if (childElements[j].textContent.includes('stackle')) {
+            if (childElements[j].textContent.toLowerCase().includes('stackle')) {
                 dialogContainers[i].classList.add('stackle_module');
+                console.log("Stackle Module Detected... Adding CSS class to dialog");
                 break;
             }
         }
@@ -27,6 +28,7 @@ const detectStackleModule = () => {
 };
 // Call the function to run the code
 $(".ui-dialog").on("dialogopen", function (event, ui) {
+    console.log("Dialog window openning!")
     detectStackleModule();
 });
 
