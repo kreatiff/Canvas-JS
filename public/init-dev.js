7aa6074c90544101b768348ff4e235356ca9d0eb
Oleg Estrin <oleg.estrin@gmail.com>
2023-03-30 16:22:28 +1000

    Watch for DOM mutations to detect dialogs

--- public/init-dev.js
+++ public/init-dev.js
@@ -27,11 +27,22 @@ const detectStackleModule = () => {
     }
 };
 // Call the function to run the code
-$(".ui-dialog").on("dialogopen", function (event, ui) {
-    console.log("Dialog window openning!")
-    detectStackleModule();
+
+// Define the observer and configure it to watch for child element additions to the document body
+const observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            console.log("Dialog window openning!")
+            checkForStackleInDialogs();
+        }
+    }
 });
 
+// Start observing the document body for changes
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
 
 loadJS(
     `https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.2/js/iframeResizer.min.js`
