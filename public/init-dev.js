846d49014a9a45b823a7268052801ebcc5c1944e
Oleg Estrin <oleg.estrin@gmail.com>
2023-03-30 21:17:27 +1000

    removing all mutation observers

--- public/init-dev.js
+++ public/init-dev.js
@@ -13,36 +13,7 @@ const addStackleClass = () => {
     }
 };
 
-const desiredDomain = 'staging.stackle.app';
 
-// Define the configuration for the MutationObserver
-const observerConfig = {
-    childList: true,
-    subtree: true
-};
-
-// Define the callback function for the MutationObserver
-const observerCallback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            const addedNodes = mutation.addedNodes;
-            for (let i = 0; i < addedNodes.length; i++) {
-                const addedNode = addedNodes[i];
-                if (addedNode.nodeName.toLowerCase() === 'iframe' && addedNode.contentWindow.location.hostname === desiredDomain) {
-                    console.log('The iframe requested ' + desiredDomain);
-                    observer.disconnect();
-                    return;
-                }
-            }
-        }
-    }
-};
-
-// Create a new MutationObserver object
-const observer = new MutationObserver(observerCallback);
-
-// Start observing changes to the DOM
-observer.observe(document.documentElement, observerConfig);
 
 
 
