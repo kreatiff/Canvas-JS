9b8c6b73cff34acc93ce578b3b1042fd0fe8b565
kreatiff <oleg.estrin@gmail.com>
2024-03-20 22:09:40 +1000

    Cidilabs CSS for DEV

--- public/init-dev.js
+++ public/init-dev.js
@@ -1,35 +1,37 @@
 const addStackleClass = () => {
-    // Get the container element with class "tool_content_wrapper"
-    const container = document.querySelector(".tool_content_wrapper");
+  // Get the container element with class "tool_content_wrapper"
+  const container = document.querySelector(".tool_content_wrapper");
 
-    // Check if the container has a child element with attribute "data-tool-id" equal to "staging.stackle.app"
-    if (container !== null) {
-        const hasStackleIntegration = container.querySelector("[data-tool-id*='stackle.app']") !== null;
+  // Check if the container has a child element with attribute "data-tool-id" equal to "staging.stackle.app"
+  if (container !== null) {
+    const hasStackleIntegration =
+      container.querySelector("[data-tool-id*='stackle.app']") !== null;
 
-        // If the container has a child element with attribute "data-tool-id" equal to "staging.stackle.app",
-        // add a class "stackle_integration" to the container element
-        if (hasStackleIntegration) {
-            document.body.classList.add("stackle_inside");
-            container.classList.add("stackle_integration");
-            console.log("Stackle Detected... Adding CSS classes");
-        }
+    // If the container has a child element with attribute "data-tool-id" equal to "staging.stackle.app",
+    // add a class "stackle_integration" to the container element
+    if (hasStackleIntegration) {
+      document.body.classList.add("stackle_inside");
+      container.classList.add("stackle_integration");
+      console.log("Stackle Detected... Adding CSS classes");
     }
+  }
 };
 
 loadJS(`${hostUrl}/moduleWindowFix.js`).then(function () {
-    console.log("Stackle Module Fix loaded");
+  console.log("Stackle Module Fix loaded");
 });
 
 loadJS(
-    `https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.2/js/iframeResizer.min.js`
+  `https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.2/js/iframeResizer.min.js`
 ).then(function () {
-    console.log("Resizer loaded");
-    iFrameResize({
-        log: false,
-        heightCalculationMethod: "max",
-        checkOrigin: false
-    });
-    addStackleClass();
+  console.log("Resizer loaded");
+  iFrameResize({
+    log: false,
+    heightCalculationMethod: "max",
+    checkOrigin: false,
+  });
+  addStackleClass();
 });
 
 loadCSS(`${hostUrl}/stackle_canvas.css`);
+loadCSS(`${hostUrl}/cdls.css`);
