27250b87a80750fb99d37295b1ab9b5019c5f097
kreatiff <oleg.estrin@gmail.com>
2024-10-15 23:42:02 +1000

    Refactor init-dev.js for improved readability by simplifying function expressions and ensuring consistent formatting throughout

--- public/init-dev.js
+++ public/init-dev.js
@@ -1,45 +1,48 @@
 const addStackleClass = () => {
   // Select all form elements with [data-tool-id*='stackle.app']
-  const stackleForms = document.querySelectorAll("form[data-tool-id*='stackle.app']");
-  console.log("Stackle Forms:",  stackleForms.length);
+  const stackleForms = document.querySelectorAll(
+    "form[data-tool-id*='stackle.app']"
+  );
+  console.log("Stackle Forms:", stackleForms.length);
   // If we found any Stackle forms
   if (stackleForms.length > 0) {
     // Add 'stackle_inside' class to the body
     document.body.classList.add("stackle_inside");
     console.log("Stackle Detected... Added CSS classes");
     // For each Stackle form
-    stackleForms.forEach(form => {
+    stackleForms.forEach((form) => {
       // Add 'stackle_integration' class to the form
       form.classList.add("stackle_integration");
 
       // Find the closest parent with class 'tool_content_wrapper'
-      const wrapper = form.closest('.tool_content_wrapper');
-      
-      // if (wrapper) {
-      //   // Find the iframe within this wrapper
-      //   const iframe = wrapper.querySelector('iframe');
-        
-      //   if (iframe) {
-      //     iframe.classList.add("stackle_iframe");
-      //   }
-      // }
+      const wrapper = form.closest(".tool_content_wrapper");
+
+      if (wrapper) {
+       // Find the iframe within this wrapper
+         const iframe = wrapper.querySelector('iframe');
+
+        if (iframe) {
+           iframe.classList.add("stackle_iframe");
+         }
+       }
     });
   }
 };
 function getIFrameThatSentMessage(message) {
   let allIFrames = Array.from(document.querySelectorAll("iframe"));
-  return allIFrames.find(
-      iframe => iframe.contentWindow == message.source
-  );
+  return allIFrames.find((iframe) => iframe.contentWindow == message.source);
 }
 function receiveMessage(event) {
-  if (event.origin.includes("stackle.app") || event.origin.includes("stacklehq.com")) { 
+  if (
+    event.origin.includes("stackle.app") ||
+    event.origin.includes("stacklehq.com")
+  ) {
     currentIframe = getIFrameThatSentMessage(event);
 
     console.log(getIFrameThatSentMessage(event));
-      //console.log("event.origin: ", event.origin);
-      //console.log("event.data: ", event.data);
-      //console.log("event.source: ", event.source.postMessage);
+    //console.log("event.origin: ", event.origin);
+    //console.log("event.data: ", event.data);
+    //console.log("event.source: ", event.source.postMessage);
   }
 }
 // event listener for message event
@@ -47,30 +50,30 @@ window.addEventListener("message", receiveMessage, false);
 
 // Get the current URL
 let currentURL = window.location.href;
-document.addEventListener('DOMContentLoaded', (event) => {
+
 // Check if the URL contains "/edit" or "/speed_grader"
-if (currentURL.includes('/edit') || currentURL.includes('/speed_grader')) {
-    console.log('URL contains /edit or /speed_grader');
+if (currentURL.includes("/edit") || currentURL.includes("/speed_grader")) {
+  console.log("URL contains /edit or /speed_grader");
 } else {
-  loadJS(
-    `${hostUrl}/iframeResizer.min.js`
-  ).then(function () {
-    addStackleClass();
-    let counter = 0;
-    const intervalId = setInterval(() => {
-      iFrameResize({
-        log: false,
-        heightCalculationMethod: "bodyScroll",
-        checkOrigin: false
-      }, '.stackle_iframe');
-      counter++;
-      if (counter === 10) {
-        clearInterval(intervalId);
-      }
-    }, 1000);
+  loadJS(`${hostUrl}/iframeResizer.min.js`).then(function () {
+    document.addEventListener("DOMContentLoaded", (event) => {
+      addStackleClass();
+      let counter = 0;
+      const intervalId = setInterval(() => {
+        iFrameResize(
+          {
+            log: false,
+            heightCalculationMethod: "bodyScroll",
+            checkOrigin: false,
+          },
+          ".stackle_iframe"
+        );
+        counter++;
+        if (counter === 10) {
+          clearInterval(intervalId);
+        }
+      }, 1000);
+    });
   });
 }
-
-
 loadCSS(`${hostUrl}/stackle_canvas.css`);
-});
