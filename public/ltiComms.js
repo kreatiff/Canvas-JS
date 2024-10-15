66c7150338173daa1d3eece5b38f734ac2e80bd6
kreatiff <oleg.estrin@gmail.com>
2024-10-15 23:32:02 +0000

    adding lti comms to send messages back to canvas

---
+++ public/ltiComms.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+	var body = document.body, html = document.documentElement;
+	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+	window.parent.postMessage(
+  {
+    subject: 'lti.frameResize',
+    height: height
+  },
+  '*'
+);
+	window.parent.postMessage({'frameUrl' : location.href}, '*');
+    console.log(location.href);
+});
