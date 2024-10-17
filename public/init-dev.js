44070ef401fd2e5bddc363799c5d35607481b984
kreatiff <oleg.estrin@gmail.com>
2024-10-17 15:41:03 +1000

    Simplify origin check in stackleLTIResizer

--- public/init-dev.js
+++ public/init-dev.js
@@ -3,11 +3,8 @@ function detectStackleIframe(message) {
   return allIFrames.find((iframe) => iframe.contentWindow == message.source);
 }
 function stackleLTIResizer(event) {
-  if (
-    event.origin.includes("stackle.app") ||
-    event.origin.includes("stacklehq.com") ||
-    event.origin.includes("stackle.test")
-  ) {
+  if (event.origin.includes("stackle.app")) 
+  {
     currentIframe = detectStackleIframe(event);
     currentIframe.classList.add("stackle_iframe");
     document.body.classList.add("stackle_inside");
