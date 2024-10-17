deb7d0ddd4ea4c89202d33459dac3fedb53b2cb9
kreatiff <oleg.estrin@gmail.com>
2024-10-17 15:28:34 +1000

    increase the height tolerance to account for outliers

--- public/ltiComms.js
+++ public/ltiComms.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 	var body = document.body, html = document.documentElement;
-	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight )+10;
+	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight )+25;
 	window.parent.postMessage(
   {
     subject: 'lti.frameResize',
