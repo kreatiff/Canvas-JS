b5302369dd7e0194ab12217e1b542affe7a4cec7
kreatiff <oleg.estrin@gmail.com>
2024-10-19 13:07:02 +1000

    Add mutation observer and debounce for iframe resizing in  ltiComms-dev.js for improved  performance and responsiveness

--- public/ltiComms-dev.js
+++ public/ltiComms-dev.js
@@ -1,8 +1,11 @@
+let isResizing = false;
+let miniApplied = false;
+
 function applyMiniCSS(event) {
-  // Check if the data in the event matches "applyMiniCSS"
-  if (event.data == "applyMiniCSS" && event.origin == "https://stackle.instructure.com") {
+  if (event.data == "applyMiniCSS" && event.origin == "https://stackle.instructure.com" && !miniApplied) {
     console.log(event);
     document.body.classList.add("stackle-mini");
+    miniApplied = true;
     
     // Wait for a short time to ensure CSS changes have been applied
     setTimeout(resizeIframe, 100);
@@ -10,6 +13,9 @@ function applyMiniCSS(event) {
 }
 
 function resizeIframe() {
+  if (isResizing) return;
+  isResizing = true;
+
   var body = document.body, html = document.documentElement;
   var height = Math.max(
     body.scrollHeight, 
@@ -30,6 +36,8 @@ function resizeIframe() {
 
   // Send the current frame URL to the parent window
   window.parent.postMessage({'frameUrl' : location.href}, '*');
+
+  setTimeout(() => { isResizing = false; }, 50);
 }
 
 // Add event listener for messages from other windows
@@ -40,4 +48,26 @@ document.onreadystatechange = function () {
   if (document.readyState == "complete") {
     resizeIframe();
   }
+}
+
+// Add a mutation observer to detect changes in the DOM
+const observer = new MutationObserver(debounce(resizeIframe, 250));
+observer.observe(document.body, { 
+  childList: true, 
+  subtree: true, 
+  attributes: true, 
+  characterData: true 
+});
+
+// Debounce function to limit how often resizeIframe is called
+function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
 }
