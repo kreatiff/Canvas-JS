fa04ef47112cb3bcbf38b3913e350f8ef24f25f0
kreatiff <oleg.estrin@gmail.com>
2024-10-19 12:47:07 +1000

    refactor applyMiniCSS function and streamline iframe resizing logic for improved clarity and functionality

--- public/ltiComms.js
+++ public/ltiComms.js
@@ -1,51 +1,38 @@
-const checkIframeSize = (wrapperElement) => {
-  // Calculate the maximum height of the document
-  if (wrapperElement == "") {
-    var body = document.body,
-      html = document.documentElement;
-    var height =
-      Math.max(
-        body.scrollHeight,
-        body.offsetHeight,
-        html.clientHeight,
-        html.scrollHeight,
-        html.offsetHeight
-      ) + 25; // Add 25px padding
-  } else {
-    var height = document.getElementById(wrapperElement) + 25;
+function applyMiniCSS(event) {
+  // Check if the data in the event matches "applyMiniCSS"
+  if (event.data == "applyMiniCSS" && event.origin == "https://stackle.instructure.com") {
+  //{
+    console.log(event);
+    document.body.classList.add("stackle-mini");
   }
-  // Send a message to the parent window to resize the frame
-  window.parent.postMessage(
-    {
-      subject: "lti.frameResize",
-      height: height,
-    },
-    "*"
-  );
+}
 
-  // Define the host URL for loading resources
-  const hostUrl = "https://canvas-js.s3.ap-southeast-2.amazonaws.com/public";
+// Add event listener for messages from other windows
+window.addEventListener("message", applyMiniCSS, false);
 
-  // Function to apply mini CSS if the origin matches
-  function applyMiniCSS(event) {
-    // Check if the data in the event matches "applyMiniCSS"
-    if (
-      event.data == "applyMiniCSS" &&
-      event.origin == "https://stackle.instructure.com"
-    ) {
-      //{
-      console.log(event);
-      // Load the CSS file from the specified URL
-      document.body.classList.add("stackle-mini");
-      checkIframeSize("embed-wrapper");
-    }
+// Event listener for when the DOM content is fully loaded
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+
+    var body = document.body, html = document.documentElement;
+    var height = Math.max(
+      body.scrollHeight, 
+      body.offsetHeight, 
+      html.clientHeight, 
+      html.scrollHeight, 
+      html.offsetHeight
+    ) + 25; // Add 25px padding
+  
+    // Send a message to the parent window to resize the frame
+    window.parent.postMessage(
+      {
+        subject: 'lti.frameResize',
+        height: height
+      },
+      '*'
+    );
+  
+    // Send the current frame URL to the parent window
+    window.parent.postMessage({'frameUrl' : location.href}, '*');
   }
-
-  // Add event listener for messages from other windows
-  window.addEventListener("message", applyMiniCSS, false);
-
-  // Send the current frame URL to the parent window
-  window.parent.postMessage({ frameUrl: location.href }, "*");
-};
-
-document.addEventListener("DOMContentLoaded", checkIframeSize(""), false);
+}
