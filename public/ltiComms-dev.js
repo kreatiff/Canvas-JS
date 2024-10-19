51c84b10a2e7d254c263b8113b21bc4eace29424
kreatiff <oleg.estrin@gmail.com>
2024-10-19 13:16:06 +1000

    Reformatting

--- public/ltiComms-dev.js
+++ public/ltiComms-dev.js
@@ -1,43 +1,46 @@
 function applyMiniCSS(event) {
-    if (event.data == "applyMiniCSS" && event.origin == "https://stackle.instructure.com") {
-      console.log(event);
-      document.body.classList.add("stackle-mini");
-      
-      // Wait for a short time to ensure CSS changes have been applied
-      setTimeout(resizeIframe, 100);
-    }
+  if (
+    event.data == "applyMiniCSS" &&
+    event.origin == "https://stackle.instructure.com"
+  ) {
+    console.log(event);
+    document.body.classList.add("stackle-mini");
+
+    // Wait for a short time to ensure CSS changes have been applied
+    setTimeout(resizeIframe, 100);
   }
-  
-  function resizeIframe() {
-    // Find the main content container
-    const contentContainer = document.querySelector('#embed-wrapper');
-    
-    if (contentContainer) {
-      // Get the height of the content container
-      const height = contentContainer.offsetHeight + 25; // Add 25px padding
-  
-      // Send a message to the parent window to resize the frame
-      window.parent.postMessage(
-        {
-          subject: 'lti.frameResize',
-          height: height
-        },
-        '*'
-      );
-    }
+}
+
+function resizeIframe() {
+  // Find the main content container
+  const contentContainer = document.querySelector("#embed-wrapper");
+
+  if (contentContainer) {
+    // Get the height of the content container
+    const height = contentContainer.offsetHeight + 25; // Add 25px padding
+
+    // Send a message to the parent window to resize the frame
+    window.parent.postMessage(
+      {
+        subject: "lti.frameResize",
+        height: height,
+      },
+      "*"
+    );
   }
-  
-  // Add event listener for messages from other windows
-  window.addEventListener("message", applyMiniCSS, false);
-  
-  // Event listener for when the DOM content is fully loaded
-  document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-      resizeIframe();
+}
+
+// Add event listener for messages from other windows
+window.addEventListener("message", applyMiniCSS, false);
+
+// Event listener for when the DOM content is fully loaded
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    resizeIframe();
     // Send the current frame URL to the parent window
-    window.parent.postMessage({'frameUrl' : location.href}, '*');
-    }
+    window.parent.postMessage({ frameUrl: location.href }, "*");
   }
-  
-  // Additional resize listener to handle dynamic content changes
-  window.addEventListener('resize', resizeIframe);
+};
+
+// Additional resize listener to handle dynamic content changes
+window.addEventListener("resize", resizeIframe);
