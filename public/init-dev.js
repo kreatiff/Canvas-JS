7599e73f835fe678d506a2e0c2b014bfc69e8239
kreatiff <oleg.estrin@gmail.com>
2024-10-19 13:24:52 +1000

    update iframe resize condition to check for "stackle-mini" class instead of width

--- public/init-dev.js
+++ public/init-dev.js
@@ -9,7 +9,7 @@ function stackleLTIResizer(event) {
     currentIframe.classList.add("stackle_iframe");
     document.body.classList.add("stackle_inside");
     console.log('Stackle embed resized successfully');
-    if (currentIframe.offsetWidth < 600) {
+    if (currentIframe.classList.contains("stackle-mini")) {
       currentIframe.contentWindow.postMessage("applyMiniCSS", "*");
       console.log("Applying mini CSS");
     }
