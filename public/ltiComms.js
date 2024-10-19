160b01de2272a470d87d3d661e05b50f34fd4af3
kreatiff <oleg.estrin@gmail.com>
2024-10-19 12:27:57 +1000

    add checkIframeSize function to communicate iframe size to parent window

--- public/ltiComms.js
+++ public/ltiComms.js
@@ -11,26 +11,6 @@ const loadCSS = (filename) => {
   // Append the link element to the head of the document
   document.getElementsByTagName("head")[0].appendChild(fileref);
 };
-
-// Define the host URL for loading resources
-const hostUrl = "https://canvas-js.s3.ap-southeast-2.amazonaws.com/public";
-
-// Function to apply mini CSS if the origin matches
-function applyMiniCSS(event) {
-  // Check if the data in the event matches "applyMiniCSS"
-  if (event.data == "applyMiniCSS" && event.origin == "https://stackle.instructure.com") {
-  //{
-    console.log(event);
-    // Load the CSS file from the specified URL
-    document.body.classList.add("stackle-mini");
-    //loadCSS(`${hostUrl}/osg_stackle_canvas.css`);
-    checkIframeSize;
-  }
-}
-
-// Add event listener for messages from other windows
-window.addEventListener("message", applyMiniCSS, false);
-
 const checkIframeSize = () => {
   // Calculate the maximum height of the document
   var body = document.body, html = document.documentElement;
@@ -50,6 +30,27 @@ const checkIframeSize = () => {
     },
     '*'
   );
+  
+// Define the host URL for loading resources
+const hostUrl = "https://canvas-js.s3.ap-southeast-2.amazonaws.com/public";
+
+// Function to apply mini CSS if the origin matches
+function applyMiniCSS(event) {
+  // Check if the data in the event matches "applyMiniCSS"
+  if (event.data == "applyMiniCSS" && event.origin == "https://stackle.instructure.com") {
+  //{
+    console.log(event);
+    // Load the CSS file from the specified URL
+    document.body.classList.add("stackle-mini");
+    //loadCSS(`${hostUrl}/osg_stackle_canvas.css`);
+    checkIframeSize;
+  }
+}
+
+// Add event listener for messages from other windows
+window.addEventListener("message", applyMiniCSS, false);
+
+
 
   // Send the current frame URL to the parent window
   window.parent.postMessage({'frameUrl' : location.href}, '*');
