5d39ed1474dff21685f808ffc81fea35dd67eb1a
kreatiff <oleg.estrin@gmail.com>
2024-10-25 12:07:19 +1000

    ...

--- public/init-dev.js
+++ public/init-dev.js
@@ -1,24 +1,34 @@
+(() => {
 function detectStackleIframe(message) {
   let allIFrames = Array.from(document.querySelectorAll("iframe"));
+  
+  // Handle stackle-mini iframes
+  allIFrames.forEach(iframe => {
+    if (iframe.classList.contains('stackle-mini')) {
+      console.log("Found stackle-mini class!");
+      iframe.contentWindow.postMessage("applyMiniCSS", "*");
+    }
+  });
+
+  // Return the matching iframe
   return allIFrames.find((iframe) => iframe.contentWindow == message.source);
 }
+
 function stackleLTIResizer(event) {
   if ((event.origin.includes("stackle.app") || 
-  event.origin.includes("stackle.test")) &&
-  event.data == "Stackle iFrame Loaded"
-  ) 
-  {
+      event.origin.includes("stackle.test")) &&
+      event.data == "Stackle iFrame Loaded"
+  ) {
     currentIframe = detectStackleIframe(event);
     currentIframe.classList.add("stackle_iframe");
     document.body.classList.add("stackle_inside");
     console.log('Stackle embed resized successfully');
-    document.querySelectorAll("iframe").forEach(iframe => {
-      if (iframe.classList.contains('stackle-mini')) {
-        console.log("Found stackle-mini class!");
-        iframe.contentWindow.postMessage("applyMiniCSS", "*");
-      }
-    });
+
+    // Remove the event listener after it's fired
+    window.removeEventListener("message", stackleLTIResizer, false);
   }
 }
-// event listener for message event
+
+// Add the event listener
 window.addEventListener("message", stackleLTIResizer, false);
+})();
