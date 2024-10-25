d74ea32ed0ffc73792e674edc3633696bac02be6
kreatiff <oleg.estrin@gmail.com>
2024-10-25 11:47:46 +1000

    Refactor stackleLTIResizer to streamline iframe  message handling

--- public/init-dev.js
+++ public/init-dev.js
@@ -3,17 +3,17 @@ function detectStackleIframe(message) {
   return allIFrames.find((iframe) => iframe.contentWindow == message.source);
 }
 function stackleLTIResizer(event) {
+  document.querySelectorAll("iframe").forEach(iframe => {
+    if (iframe.classList.contains('stackle-mini')) {
+      console.log("Found stackle-mini class!");
+      iframe.contentWindow.postMessage("applyMiniCSS", "*");
+    }
+  });
   if ((event.origin.includes("stackle.app") || 
   event.origin.includes("stackle.test")) &&
   event.data == "Stackle iFrame Loaded"
   ) 
   {
-    document.querySelectorAll("iframe").forEach(iframe => {
-      if (iframe.classList.contains('stackle-mini')) {
-        console.log("Found stackle-mini class!");
-        iframe.contentWindow.postMessage("applyMiniCSS", "*");
-      }
-    });
     currentIframe = detectStackleIframe(event);
     currentIframe.classList.add("stackle_iframe");
     document.body.classList.add("stackle_inside");
