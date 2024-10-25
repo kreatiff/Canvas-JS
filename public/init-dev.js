0575958d213b01aa37f7c8d1274c7c1102df298a
kreatiff <oleg.estrin@gmail.com>
2024-10-25 11:13:20 +1000

    Simplify iframe class check logic

--- public/init-dev.js
+++ public/init-dev.js
@@ -17,25 +17,20 @@ function stackleLTIResizer(event) {
     let checkCount = 0;
     const checkInterval = setInterval(() => {
       console.log("Checking for stackle-mini class...");
-      document.onreadystatechange = function () {
-        if (document.readyState == "complete") {
-          console.log("Parent iframe ready");
-          if (window.frameElement && window.frameElement.classList.contains('stackle-mini') &&) {
-            console.log(window.frameElement);
-            window.frameElement.contentWindow.postMessage("applyMiniCSS", "*");
-            console.log("Applying mini CSS to parent iframe");
-            currentIframe.contentWindow.postMessage("applyMiniCSS", "*");
-            console.log("Applying mini CSS");
-            clearInterval(checkInterval);
-          }else{
-            checkCount++;
-            if (checkCount >= 10) { // Stop checking after 10 attempts
-              console.log("Class check timed out");
-              clearInterval(checkInterval);
-            }
-          }
+      if ((window.frameElement && window.frameElement.classList.contains('stackle-mini')) || currentIframe.classList.contains('stackle-mini')) {
+        console.log(window.frameElement);
+        window.frameElement.contentWindow.postMessage("applyMiniCSS", "*");
+        console.log("Applying mini CSS to parent iframe");
+        currentIframe.contentWindow.postMessage("applyMiniCSS", "*");
+        console.log("Applying mini CSS");
+        clearInterval(checkInterval);
+      }else{
+        checkCount++;
+        if (checkCount >= 5) { // Stop checking after 10 attempts
+          console.log("Class check timed out");
+          clearInterval(checkInterval);
         }
-      };
+      }
     }, 200); // Check every 200ms
   }
 }
