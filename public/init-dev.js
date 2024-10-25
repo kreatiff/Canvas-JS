3b27088249f9eb1e9a8d8db715ebea6619ec6347
kreatiff <oleg.estrin@gmail.com>
2024-10-25 12:37:59 +1000

    Rolling back

--- public/init-dev.js
+++ public/init-dev.js
@@ -1,34 +1,29 @@
-(() => {
 function detectStackleIframe(message) {
   let allIFrames = Array.from(document.querySelectorAll("iframe"));
-  // Return the matching iframe
   return allIFrames.find((iframe) => iframe.contentWindow == message.source);
 }
-
 function stackleLTIResizer(event) {
   if ((event.origin.includes("stackle.app") || 
-      event.origin.includes("stackle.test")) &&
-      event.data == "Stackle iFrame Loaded"
-  ) {
+  event.origin.includes("stackle.test")) &&
+  event.data == "Stackle iFrame Loaded"
+  ) 
+  {
     currentIframe = detectStackleIframe(event);
     currentIframe.classList.add("stackle_iframe");
     document.body.classList.add("stackle_inside");
     console.log('Stackle embed resized successfully');
-    stackleMinifier();
+    
+    // Check if parent iframe has stackle-mini class
+    if (window.frameElement && window.frameElement.classList.contains('stackle-mini')) {
+      console.log(window.frameElement);
+      window.frameElement.contentWindow.postMessage("applyMiniCSS", "*");
+      console.log("Applying mini CSS to parent iframe");
+      currentIframe.contentWindow.postMessage("applyMiniCSS", "*");
+      console.log("Applying mini CSS");
+    }else{
+      console.log("No stackle-mini class found");
+    }
   }
 }
-function stackleMinifier() {
-    // Handle stackle-mini iframes
-    iFrames= document.querySelectorAll("iframe");
-    console.log("Looking for mini-stackle iframes...");
-    iFrames.forEach(iframe => {
-      if (iframe.classList.contains('stackle-mini')) {
-        console.log("Found stackle-mini class!");
-        iframe.contentWindow.postMessage("applyMiniCSS", "*");
-      }
-    });
-}
-// Add the event listener
-document.addEventListener("DOMContentLoaded", stackleMinifier);
+// event listener for message event
 window.addEventListener("message", stackleLTIResizer, false);
-})();
